e9e91643cf0590ac86926e132df66b2cfa8658c0
Mendess2526 <pedro.mendes.26@gmail.com>
2019-10-24 13:24:56 +0100

    Base server

---
+++ server/src/main.rs
@@ -0,0 +1,77 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+mod model;
+#[macro_use]
+extern crate rocket;
+
+use model::{ChildId, Db, GuardianId, Location};
+use rocket::State;
+use rocket_contrib::json::Json;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct RegisterGuardianRequest {
+    username: String,
+    password: Vec<u8>,
+}
+
+#[post("/guardian/create", data = "<request>")]
+fn register_guardian(db: State<Db>, request: Json<RegisterGuardianRequest>) -> String {
+    let request = request.into_inner();
+    db.register_new_guardian(request.username, request.password)
+        .to_string()
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct GetChildLocation {
+    guardian: GuardianId,
+    child: ChildId,
+}
+
+#[post("/guardian", data = "<request>")]
+fn where_is_my_child(db: State<Db>, request: Json<GetChildLocation>) -> Option<Json<Location>> {
+    db.child_location(request.child, request.guardian)
+        .map(|l| Json(l))
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct RegisterChild {
+    guardian: GuardianId,
+}
+
+#[post("/child/create", data = "<request>")]
+fn register_child(db: State<Db>, request: Json<RegisterChild>) -> String {
+    db.register_new_child(request.guardian).to_string()
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ChildLocation {
+    location: Location,
+    child: ChildId,
+}
+
+#[post("/child", data = "<request>")]
+fn update_child_location(db: State<Db>, request: Json<ChildLocation>) {
+    db.update_child_location(request.child, request.location)
+}
+
+#[get("/state")]
+fn print_state(db: State<Db>) -> String {
+    format!("{:#?}", db)
+}
+
+fn main() {
+    rocket::ignite()
+        .mount(
+            "/",
+            routes![
+                where_is_my_child,
+                register_guardian,
+                register_child,
+                update_child_location,
+                print_state,
+            ],
+        )
+        .manage(Db::default())
+        .launch();
+}
---
+++ server/src/model.rs
@@ -0,0 +1,69 @@
+mod child;
+mod guardian;
+
+use child::Child;
+use guardian::Guardian;
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, sync::Mutex};
+
+pub use child::ChildId;
+pub use guardian::GuardianId;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub struct Location {
+    x: i64,
+    y: i64,
+}
+
+#[derive(Default, Debug)]
+pub struct Db {
+    children: Mutex<HashMap<ChildId, Child>>,
+    guardians: Mutex<HashMap<GuardianId, Guardian>>,
+}
+
+impl Db {
+    pub fn register_new_guardian(&self, username: String, password: Vec<u8>) -> GuardianId {
+        let new = Guardian::new(username, password);
+        let id = new.id();
+        self.guardians.lock().unwrap().insert(id, new);
+        id
+    }
+
+    pub fn register_new_child(&self, guardian: GuardianId) -> ChildId {
+        let new = Child::new(guardian);
+        let id = new.id();
+        self.children.lock().unwrap().insert(id, new);
+        id
+    }
+
+    pub fn child_location(&self, child: ChildId, guardian: GuardianId) -> Option<Location> {
+        self.children
+            .lock()
+            .unwrap()
+            .get(&child)
+            .and_then(|c| c.location(guardian))
+    }
+
+    pub fn update_child_location(&self, child: ChildId, location: Location) {
+        self.children
+            .lock()
+            .unwrap()
+            .get_mut(&child)
+            .map(|c| c.update_location(location));
+    }
+
+    /// This is an angle of attack
+    /// If an attacker gets a hold of the child's phone they can easily add
+    /// themselves as a guardian.
+    ///
+    /// New guardians need to have the shared secret first before
+    /// being regitered here.
+    pub fn guard_child(&self, child: ChildId, guardian: GuardianId) -> bool {
+        self.children
+            .lock()
+            .unwrap()
+            .get_mut(&child)
+            .map(|c| c.add_guardian(guardian))
+            .is_some()
+    }
+}
