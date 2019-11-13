7c7c46d17c2533cf35836e694a1f9d4e8f9ba1e3
FelipeGorostiaga <fgorostiaga@itba.edu.ar>
2019-11-13 18:29:54 +0000

    Advances in Google auth

--- Guardian/app/src/main/java/com/sirs/guardianapp/MainActivity.java
+++ Guardian/app/src/main/java/com/sirs/guardianapp/MainActivity.java
@@ -1,14 +1,42 @@
 package com.sirs.guardianapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button signOutButton;
+    Button signInButton;
+    TextView statusTextView;
+    //private GoogleSignInClient mGoogleSignInClient;
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                //change this!
+                .requestIdToken("default web client id")
+                .requestEmail()
+                .build();
+        //mGoogleSignInClient = GoogleSignIn.getClient(this, gso);x
     }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
 }
