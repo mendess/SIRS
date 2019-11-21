b9da900394ed99ec13c71fcf971a1f2bc2591572
Eva Verboom <e.verboom@student.tudelft.nl>
2019-11-21 15:26:06 +0000

    Adds SOS method

--- SpyKid/child/src/main/java/sirs/spykid/child/Beacon.kt
+++ SpyKid/child/src/main/java/sirs/spykid/child/Beacon.kt
@@ -7,7 +7,7 @@ import java.io.*
 import java.net.URL
 import javax.crypto.SecretKey
 
-class Beacon : AsyncTask<Unit, Unit, Unit>() {
+class Beacon : AsyncTask<Pair<Location, SecretKey>, Unit, Unit>() {
 
     private lateinit var mLocation : Location
     private lateinit var secretKey : SecretKey
@@ -30,7 +30,7 @@ class Beacon : AsyncTask<Unit, Unit, Unit>() {
             println("opening connection")
             val connection = url.openConnection()
             connection.doOutput = true
-            val locationMessage = ("{ \"child\": 0, \"location\": " + getLocation() + "}}").toByteArray()
+            val locationMessage = ("{ \"child\": 0, \"location\": " + getLocation() + "}").toByteArray()
             val encryptedMessage = encryptionAlgorithm.encrypt(secretKey, locationMessage)
             connection.getOutputStream().write(encryptedMessage)
             connection.connect()
@@ -49,8 +49,34 @@ class Beacon : AsyncTask<Unit, Unit, Unit>() {
         mLocation = location
     }
 
-    override fun doInBackground(vararg params: Unit?) {
+    override fun doInBackground(vararg params: Pair<Location, SecretKey>?) {
         beacon()
     }
+
+}
+
+class SOS : AsyncTask<SecretKey, Unit, Unit>() {
+
+    private val encryptionAlgorithm = EncryptionAlgorithm()
+
+    private fun sendSOS(secretKey: SecretKey) {
+        while (true) {
+            val url = URL("http://localhost:8000/child")
+            println("opening connection")
+            val connection = url.openConnection()
+            connection.doOutput = true
+            val locationMessage = ("{ \"child\": 0, \"location\": SOS}").toByteArray()
+            val encryptedMessage = encryptionAlgorithm.encrypt(secretKey, locationMessage)
+            connection.getOutputStream().write(encryptedMessage)
+            connection.connect()
+            println(BufferedInputStream(connection.getInputStream()).reader().readText())
+            Thread.sleep(1000)
+        }
+    }
+
+    override fun doInBackground(vararg params: SecretKey) {
+        sendSOS(params[0])
+    }
+
 
 }
