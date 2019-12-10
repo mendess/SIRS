3a6f901b5733c8adafb56145a4293fe56066ad90
FelipeGorostiaga <fgorostiaga@itba.edu.ar>
2019-12-10 15:21:37 +0000

    Advances in beacon location requests

--- SpyKid/child/src/main/java/sirs/spykid/child/activity/BeaconActivity.kt
+++ SpyKid/child/src/main/java/sirs/spykid/child/activity/BeaconActivity.kt
@@ -2,12 +2,13 @@ package sirs.spykid.child.activity
 
 import android.os.Build
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.*
 import sirs.spykid.child.R
 import sirs.spykid.util.EncryptionAlgorithm
 import sirs.spykid.util.Location
@@ -18,6 +19,12 @@ import java.util.function.Consumer
 
 @RequiresApi(Build.VERSION_CODES.O)
 class BeaconActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: android.location.Location
+    private lateinit var locationRequest : LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_beacon)
@@ -25,8 +32,42 @@ class BeaconActivity : AppCompatActivity() {
             EncryptionAlgorithm.deleteKey(EncryptionAlgorithm.SHARED_SECRET_NAME)
             finish()
         }
-        startLocationBroadcaster()
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = LocationRequest.create()?.apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }!!
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations){
+
+                    //TODO send locations to server (they come with lat, long and timestamp)
+
+                }
+            }
+        }
+        startLocationUpdates()
+
+
+      /*  fusedLocationClient.lastLocation.addOnSuccessListener { location : android.location.Location? ->
+            if (location != null) {
+                lastLocation = location
+            }
+        }*/
     }
+
+    private fun startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+                locationCallback,
+                Looper.getMainLooper())
+    }
+
+
+
 
     private fun startLocationBroadcaster() {
         // Calling an async task inside another async task is a deadlock, so we use a thread
@@ -59,4 +100,8 @@ class BeaconActivity : AppCompatActivity() {
         val random = Random()
         return Location(random.nextDouble() * 100, random.nextDouble() * 100, LocalDateTime.now())
     }
+
+
+
+
 }
